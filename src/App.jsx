71fad5c9559cf2183c4a16c87fff21f2d93b4c8e
Phung Hoang Tu <thocodeonline@gmail.com>
2025-04-23 23:18:49 +0700

    ui login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Routes from './routes';
+import './styles/App.css'
+import "./styles/responsive.css";
+import "./styles/product.css";
 
 const App = () => {
   return (
--- src/pages/Admin/Login.jsx
+++ src/pages/Admin/Login.jsx
@@ -1,10 +1,126 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import Logo from "../../assets/images/logo.png";
+
+import patern from "../../assets/images/patern.jpg";
+import googleIcon from "../../assets/images/googleIcon.png";
+
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [inputIndex, setInputIndex] = useState(null);
+
+  const [isShowPassword, setisShowPassword] = useState(false);
+
+  const focusInput = (index) => {
+    setInputIndex(index);
+  };
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Welcome to the Login Page</h1>
-    </div>
+    <>
+      <img src={patern} className="loginPatern" alt="" />
+      <section className="loginSection">
+        <div className="loginBox">
+          <div className="logo text-center">
+            <img src={Logo} width="60px" alt="" />
+            <h5 className="font-weight-bold">Login to Hotash</h5>
+          </div>
+
+          <div className="wrapper mt-3 card border ">
+            <form>
+              <div
+                className={`form-group position-relative ${
+                  inputIndex === 0 && "focus"
+                }`}
+              >
+                <span className="icon">
+                  <MdEmail />
+                </span>
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="form-control"
+                  placeholder="enter your email"
+                  onFocus={() => focusInput(0)}
+                  onBlur={() => setInputIndex(null)}
+                />
+                 {usernameError && <p style={{ color: 'red' }}>{usernameError}</p>}
+              </div>
+              <div
+                className={`form-group position-relative ${
+                  inputIndex === 1 && "focus"
+                }`}
+              >
+                <span className="icon">
+                  <RiLockPasswordFill />
+                </span>
+                <input
+                  type={`${isShowPassword === true ? "text" : "password"}`}
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="enter your password"
+                  onFocus={() => focusInput(1)}
+                  onBlur={() => setInputIndex(null)}
+                />
+                <span
+                  className="toggleShowPassword"
+                  onClick={() => setisShowPassword(!isShowPassword)}
+                >
+                  {isShowPassword === true ? <IoMdEyeOff /> : <IoMdEye />}
+                </span>
+                {passwordError && <p style={{ color: 'red' }}>{passwordError}</p>}
+              </div>
+
+              <div className="form-group">
+                <Button className="btn-blue btn-lg w-100 btn-big" type="submit">
+                  Sign In
+                </Button>
+              </div>
+
+              <div className="form-group text-center mb-0">
+                <Link to={"/forgot-password"} className="link">
+                  FORGOT PASSWORD
+                </Link>
+                <div className="d-flex align-items-center justify-content-center or mt-3 mb-3">
+                  <span className="line"></span>
+                  <span className="txt">or</span>
+                  <span className="line"></span>
+                </div>
+
+                <Button
+                  variant="outlined"
+                  className="w-100 btn-lg btn-big loginWithGoogle"
+                >
+                  <img src={googleIcon} width="25px" alt="" /> &nbsp; Sign In
+                  with Google
+                </Button>
+              </div>
+            </form>
+          </div>
+
+          <div className="wrapper mt-3 card border footer p-3">
+            <span className="text-center">
+              Don't have an account?
+              <Link to={"/signUp"} className="link color ml-2">
+                Register
+              </Link>
+            </span>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
